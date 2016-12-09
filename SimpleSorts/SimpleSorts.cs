c3004ba80f4a0019be872c882f631dfe08c2fe67
Jarosław Rosiak <jaroslaw.rosiak@delaval.com>
2016-12-09 17:20:45 +0100

    SimpleSorts-SelectionSort added

---
+++ SimpleSorts/SimpleSorts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortingAlgorithms
+{
+    public static class SimpleSorts
+    {
+        public static IEnumerable<T> SelectionSort<T>(IEnumerable<T> numbersToSort) where T : IComparable
+        {
+            IList<T> numbers = numbersToSort.ToList();
+            int minKey, size = numbersToSort.Count();
+            T tmp;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                minKey = i;
+
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (numbers[minKey].CompareTo(numbers[j]) < 0)
+                    {
+                        minKey = j;
+                    }
+                }
+
+                tmp = numbers[minKey];
+                numbers[minKey] = numbers[i];
+                numbers[i] = tmp;
+            }
+
+            return numbers;
+        }
+    }
+}
